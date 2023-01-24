c423d609170eadcdaeb9864881b631f3e576d02d
Lucao <lukaspc2019@gmail.com>
2023-01-24 13:11:55 -0300

    Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>AL.News</title>
+                <title>Home | AL.News</title>
             </Head>
-            <h1>
-                Hello World
-            </h1>
+           <main className={styles.contentContainer}>
+            <section className={styles.hero}>
+                <span> 👏 Hey, Welcome</span>
+                <h1>News about the <span>React</span> world.</h1>
+                <p>
+                    Get access to all publications <br/>
+                    <span>For $9.90 month</span>
+                </p>
+            </section>
+            <img src="/images/avatar.svg" alt="Girl Coding" />
+           </main>
         </>
     )
 }
