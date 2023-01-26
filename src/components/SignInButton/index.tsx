83b337d806ece833a95e98ef1cc36ae48067f96e
Lucao <lukaspc2019@gmail.com>
2023-01-26 15:08:56 -0300

    FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,19 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
     const { data: session } = useSession()
-
-    console.log(session);
     
     return session ? (
         <button 
         type = "button"
         className = {styles.signInButton}
+        onClick={() => signOut() }
     >
         <FaGithub color='#04d361' />
-        Carvalho-Lucas
+        {session.user.name}
         <FiX color='#737380' className={styles.closeIcon}/>
     </button >
     ) : (
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,35 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import Provider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
 
 export const authOptions = {
   providers: [
-    GithubProvider({
+    Provider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params:{
+          scope: 'read-user',
+        },
+      },
     }),
-    
   ],
+  callbacks: {
+    async signIn({ user, account, profile}){
+      const { email } = user;
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {
+            data: {
+              email: email,
+            },
+          },
+        )
+      )
+      return true
+    },
+  },
 }
 export default NextAuth(authOptions)
