d6ba05a4e1d0584b60feefb276cb50ce4e71175b
Lucao <lukaspc2019@gmail.com>
2023-01-30 18:11:49 -0300

    verificação duplicidade FaunaDB

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -7,14 +7,17 @@ import { fauna } from "../../services/fauna";
 type User = {
     ref: {
         id: string;
-    }   
+    }
+    data: {
+        stripe_customer_id: string
+    }
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
-    if(req.method === 'POST'){
+    if (req.method === 'POST') {
 
-        const session = await getSession ({ req });
+        const session = await getSession({ req });
 
         const user = await fauna.query<User>(
             q.Get(
@@ -24,37 +27,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                 )
             )
         )
-        
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            //metadata
-        })
 
-        await fauna.query(
-            q.Update(
-                q.Ref( q.Collection('users'), user.ref.id),
-                {
-                    data: {
-                       stripe_customer_id: stripeCustomer.id, 
-                      }
-                }
+        let customerId = user.data.stripe_customer_id
+
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                //metadata
+            })
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id,
+                        }
+                    }
+                )
             )
-        )
+            customerId = stripeCustomer.id
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
-            line_items:[
-                { price: 'price_1MTuh0GCeiJTiSjIDuYwDa9f', quantity: 1}
+            line_items: [
+                { price: 'price_1MTuh0GCeiJTiSjIDuYwDa9f', quantity: 1 }
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
             success_url: process.env.STRIPE_SUCESS_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
-        return res.status(200).json({sessionId: stripeCheckoutSession.id})
-    }else{
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+    } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
     }
